346040d2d62055856136f27d9bb2dce0927daaab
Brian Kan <BrianKan00@hotmail.com>
2019-05-22 19:00:33 -0400

    Adding Pseudo code which is writte on the JS file

---
+++ script.js
@@ -0,0 +1,35 @@
+// PSEUDO CODE
+
+// MVP
+// Add Edamam API
+// include: q as part of API
+// create drop-down menu to filter out search results.
+// Drop down includes: 
+// --- Beef
+// --- Chicken
+// --- Fish
+// --- Vegetables
+// limit search results to 5 results
+// append the following to the DOM: 
+// - Image
+// - Name of recipe
+// - Health labels and cautions 
+// - quick look up link/url to recipe.
+
+
+// STRETCH GOALS
+// create search bar on DOM.  Searches can be by recipe name or ingredients
+// on user input for search field, attach api from search bar.
+// Filter for health.  Example: calories, allergies (peanuts)
+// Filter for type of meal.  Example: breakfast, lunch, dinner
+// Add the ability to create a profile
+// Add the option to favourite recipes
+// Add the option for a search history
+// searc + // Add error handling
+
+
+const recipeApp = {};
+
+
+
+// this is done via ajax call to API, which passes the search to the query portion
